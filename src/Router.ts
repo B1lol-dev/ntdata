df7567160017b5f959aa6973a7758c3799d1dcf6
B1lol-dev <sardorovichgamer@gmail.com>
2025-04-29 00:01:00 +0500

    added Posts page

--- src/Router.ts
+++ src/Router.ts
@@ -4,6 +4,7 @@ import { createRouter } from "routerjs";
 import { Home } from "./pages/Home/Home";
 import { NotFound } from "./pages/404/NotFound";
 import { Comments } from "./pages/Comments/Comments";
+import { Posts } from "./pages/Posts/Posts";
 
 export const Router = (root: HTMLDivElement) => {
   createRouter()
@@ -12,6 +13,9 @@ export const Router = (root: HTMLDivElement) => {
     })
     .get("/comments", () => {
       root.innerHTML = Comments();
+    })
+    .get("/posts", () => {
+      root.innerHTML = Posts();
     })
     .error(404, () => {
       root.innerHTML = NotFound();
---
+++ src/pages/Posts/Posts.ts
@@ -0,0 +1,5 @@
+export const Posts = (): string => {
+  location.pathname = "/";
+
+  return /*html*/ ``;
+};
